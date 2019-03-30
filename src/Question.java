64e627f63bf6f7c9774a21518f79db76413adafb
varnit05 <League@AdministorsiMac.attlocal.net>
2019-03-29 18:27:48 -0700

    March 29

--- src/Question.java
+++ src/Question.java
@@ -1,26 +1,21 @@
 
 public class Question {
-	String answers[] = new String[4];
-	int correct;
-		public Question(String[4] answers, int correct) {
-	Question questions[];
-	this.correct = correct;
-	this.answers = answers;
-		// TODO Auto-generated constructor stub
-String[] answers = {"1", "2", "3", "4"};
-answers[0] = "1";
-		questions[0] = new Question(answers, 3);
-		questions[1] = new Question(answers, 2);
-		questions[2] = new Question(answers, 3);
-		questions[3] = new Question(answers, 3);
-		questions[4] = new Question(answers, 3);
-		questions[5] = new Question(answers, 3);
-		questions[6] = new Question(answers, 3);
-		questions[7] = new Question(answers, 3);
-		questions[8] = new Question(answers, 3);
-		questions[9] = new Question(answers, 3);
-		questions[10] = new Question(answers, 3);
-		questions[11] = new Question(answers, 3);
-	}
+		String answers[] = {"1", "2", "3", "4", "5", "6"};
+		String wrongAns[] = {"1",
+							"2", 
+							"3",
+							"4", 
+							"5",
+							"6",
+							"7",
+							"8"};
+			public Question(int wrongAmount, String wrongAns, String awnswers) {
+				// TODO Auto-generated constructor stub
+				Question questions[];
+				this.wrongAns = wrongAns;
+				this.answers = answers;
+			}
+		}
 
-}
+
+
--- src/QuestionManager.java
+++ src/QuestionManager.java
@@ -1,4 +1,22 @@
+	public class QuestionManager {
+		String correct;
+			public QuestionManager(String[4] answers, int correct) {
+		
+		
+			// TODO Auto-generated constructor stub
+	answers[0] = "1";
+			questions[0] = new Question(answers, 3);
+			questions[1] = new Question(answers, 2);
+			questions[2] = new Question(answers, 3);
+			questions[3] = new Question(answers, 3);
+			questions[4] = new Question(answers, 3);
+			questions[5] = new Question(answers, 3);
+			questions[6] = new Question(answers, 3);
+			questions[7] = new Question(answers, 3);
+			questions[8] = new Question(answers, 3);
+			questions[9] = new Question(answers, 3);
+			questions[10] = new Question(answers, 3);
+			questions[11] = new Question(answers, 3);
+		}
 
-public class QuestionManager {
-
-}
+	}
