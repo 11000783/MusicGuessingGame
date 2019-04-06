708825dbf50bbeda87a0bb51fd2706cd3c31c5ae
varnit05 <League@AdministorsiMac.attlocal.net>
2019-04-05 18:30:51 -0700

    April 5
    :sunglasses:
    Ask Jackie to help with the sets and set the wrong answers and create a
    array of questions and make it so that the game will call random
    questions, etc

--- src/Question.java
+++ src/Question.java
@@ -1,21 +1,32 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class Question {
-		String answers[] = {"1", "2", "3", "4", "5", "6"};
-		String wrongAns[] = {"1",
-							"2", 
-							"3",
-							"4", 
-							"5",
-							"6",
-							"7",
-							"8"};
-			public Question(int wrongAmount, String wrongAns, String awnswers) {
-				// TODO Auto-generated constructor stub
-				Question questions[];
-				this.wrongAns = wrongAns;
-				this.answers = answers;
+		String songs[] = {"1", "2", "3", "4", "5", "6"};
+		Random random = new Random();
+		int wrongAns1;
+		int wrongAns2;
+		int wrongAns3;
+		int rightAns;
+Set answers = new HashSet<Integer>();
+		public Question(int rightAns) {
+		
+			answers.add(rightAns);
+			while (answers.size()<4) {
+				answers.add(random.nextInt(songs.length));
 			}
-		}
+			Integer[] ints = (Integer[])answers.toArray();
+			wrongAns1 = ints[1];
+			wrongAns2 = ints[2];
+			wrongAns3 = ints[3];
+			
+			// TODO Auto-generated constructor stub
+					Question questions[];
+			
+		
+}
 
+}
 
-
+//
---
+++ src/Tester.java
@@ -0,0 +1,14 @@
+
+public class Tester {
+public static void main(String[] args) {
+	Question question = new Question(4);
+	for (Object i : question.answers) {
+		System.out.println(i);
+	}
+}
+	public Tester() {
+		// TODO Auto-generated constructor stub
+
+	}
+
+}
