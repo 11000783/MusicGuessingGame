e06d4f7fc648689389780cf8e585fd5822dc51f5
League Student <league@iMac.attlocal.net>
2019-05-17 18:34:47 -0700

    another

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -74,18 +74,18 @@ JLabel label = new JLabel("What is the song?");
 
 	}
 
-	void drawMenuState(Graphics g) {
-	
-		System.out.println("menu");
-		g.setColor(pink);
-		g.fillRect(0, 0, 600, 800);
-		g.setColor(blue);
-		g.setFont(titleFont);
-		g.drawString("Music Guessing Game", 34, 200);
-		g.setFont(instFont);
-		g.drawString("Press ENTER to start", 105, 300);
-		g.drawString("Press SPACE for instructions", 60, 400);
-	}
+//	void drawMenuState(Graphics g) {
+//	
+//		System.out.println("menu");
+//		g.setColor(pink);
+//		g.fillRect(0, 0, 600, 800);
+//		g.setColor(blue);
+//		g.setFont(titleFont);
+//		g.drawString("Music Guessing Game", 34, 200);
+//		g.setFont(instFont);
+//		g.drawString("Press ENTER to start", 105, 300);
+//		g.drawString("Press SPACE for instructions", 60, 400);
+//	}
 
 	void drawGameState() {
 		System.out.println("game");
@@ -175,6 +175,7 @@ JLabel label = new JLabel("What is the song?");
 				button2.addMouseListener(this);
 				button3.addMouseListener(this);
 				button4.addMouseListener(this);
+		
 			}
 			repaint();
 		}
