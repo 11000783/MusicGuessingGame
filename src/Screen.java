ccd82283bc60e3c6704ceff10ef9d060058e927b
League Student <league@iMac.attlocal.net>
2019-11-22 18:28:04 -0800

    :sunglasses:

--- src/Screen.java
+++ src/Screen.java
@@ -22,9 +22,7 @@ public class Screen extends JPanel {
     static final int WIDTH = 600;
     static final int HEIGHT = 800;
     
-    public BufferedImage spaceShip;
-    public BufferedImage alien;
-    public BufferedImage alienSmall;
+
     
     JFrame frame;
     GridBagConstraints c;
@@ -40,13 +38,6 @@ public class Screen extends JPanel {
         
         frame.setPreferredSize( new Dimension( Screen.WIDTH, Screen.HEIGHT ) );
         
-        try {
-            spaceShip = ImageIO.read(this.getClass().getResourceAsStream("shipUp.png"));
-            alien = ImageIO.read(this.getClass().getResourceAsStream("enemy.png"));
-            alienSmall = ImageIO.read(this.getClass().getResourceAsStream("enemy_small.png"));
-        } catch( IOException e ) {
-            System.out.println( "ERROR: unable to load image files" );
-        }
         
         // Default panel layout, can be changed
         this.setLayout( new GridBagLayout() );
